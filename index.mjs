991c06f3c6f064ec3fe0e6c3365e3cb0c0a12bb7
boogerlad <boogerlad@gmail.com>
2021-08-12 13:28:56 -0400

    cleanup and mock market orders

--- index.mjs
+++ index.mjs
@@ -1,6 +1,8 @@
 "use strict";
 import fetch from 'node-fetch';
 
+const pollingInterval = 1000;
+
 async function kraken() {
 	let result = await (await fetch('https://api.kraken.com/0/public/Depth?pair=XBTUSD')).json();
 	return {
@@ -38,18 +40,30 @@ async function coinbase() {
 }
 
 function best(books) {
-	let bestBid = {name: books[0].name, price: Number(books[0].bids[0][0])};
-	let bestAsk = {name: books[0].name, price: Number(books[0].asks[0][0])};
+	let bestBid = {name: books[0].name, price: Number(books[0].bids[0][0]), amount: Number(books[0].bids[0][1])};
+	let bestAsk = {name: books[0].name, price: Number(books[0].asks[0][0]), amount: Number(books[0].asks[0][1])};
 	for(let i = 1; i < books.length; ++i) {
-		if(Number(books[i].bids[0][0]) > bestBid.price) {
-			bestBid = {name: books[i].name, price: Number(books[i].bids[0][0])}
+		let bidPrice = Number(books[i].bids[0][0]);
+		if(bidPrice > bestBid.price) {
+			bestBid = {name: books[i].name, price: bidPrice, amount: Number(books[0].bids[0][1])};
 		}
-		if(Number(books[i].asks[0][0]) < bestAsk.price) {
-			bestAsk = {name: books[i].name, price: Number(books[i].asks[0][0])}
+		let askPrice = Number(books[i].asks[0][0]);
+		if(askPrice < bestAsk.price) {
+			bestAsk = {name: books[i].name, price: askPrice, amount: Number(books[0].asks[0][1])};
 		}
 	}
 	return [bestBid, bestAsk];
 }
+
+async function placeMarketOrder(amount, where, type) {
+	console.log(amount, where, type);
+	//assume always successful for now
+	//need to keep track of how much bought / sold to rebalance accounts
+	//format amount to correct number of digits
+	//use big.js to avoid floating point imprecision issues
+}
+
+
 
 async function main() {
 	let books = await Promise.all([
@@ -61,8 +75,14 @@ async function main() {
 	const [bestBid, bestAsk] = best(books);
 	if(bestAsk.price < bestBid.price) {
 		console.log('arbitrage opportunity @', bestAsk, bestBid);
+		let amount = Math.min(bestAsk.amount, bestBid.amount);
+		//technically amount doesn't matter since fee is percentage of amount, so just transact more often?
+		await Promise.all([
+			placeMarketOrder(amount * bestAsk.price, bestAsk.name, 'buy'),
+			placeMarketOrder(amount, bestBid.name, 'sell')
+		]);
 	}
-	setTimeout(main, 1000);
+	setTimeout(main, pollingInterval);
 }
 
 main();
