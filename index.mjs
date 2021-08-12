9c3f1f4d6b0503fea5c957b3137708c0a52090cf
boogerlad <boogerlad@gmail.com>
2021-08-12 12:50:10 -0400

    refactor

--- index.mjs
+++ index.mjs
@@ -1,66 +1,64 @@
 "use strict";
 import fetch from 'node-fetch';
 
-let bids = {};
-let asks = {};
-//todo: make functions return bids and asks to avoid global variable. then again, this is miniscually faster
 async function kraken() {
 	let result = await (await fetch('https://api.kraken.com/0/public/Depth?pair=XBTUSD')).json();
-	bids.kraken = result.result.XXBTZUSD.bids;
-	asks.kraken = result.result.XXBTZUSD.asks;
+	return {
+		name: 'kraken',
+		bids: result.result.XXBTZUSD.bids,
+		asks: result.result.XXBTZUSD.asks
+	};
 }
 
 async function ftx() {
 	let result = await (await fetch('https://ftx.com/api/markets/btc_usd/orderbook')).json();
-	bids.ftx = result.result.bids;
-	asks.ftx = result.result.asks;
+	return {
+		name: 'ftx', 
+		bids: result.result.bids,
+		asks: result.result.asks
+	};
 }
 
 async function binance() {
 	let result = await (await fetch('https://api.binance.com/api/v3/depth?symbol=BTCUSDT')).json();
-	bids.binance = result.bids;
-	asks.binance = result.asks;
+	return {
+		name: 'binance',
+		bids: result.bids,
+		asks: result.asks
+	};
 }
 
 async function coinbase() {
 	let result = await (await fetch('https://api-public.sandbox.pro.coinbase.com/products/BTC-USD/book?level=2')).json();
-	bids.coinbase = result.bids;
-	asks.coinbase = result.asks;
+	return {
+		name: 'coinbase',
+		bids: result.bids,
+		asks: result.asks
+	};
+}
+
+function best(books) {
+	let bestBid = {name: books[0].name, price: Number(books[0].bids[0][0])};
+	let bestAsk = {name: books[0].name, price: Number(books[0].asks[0][0])};
+	for(let i = 1; i < books.length; ++i) {
+		if(Number(books[i].bids[0][0]) > bestBid.price) {
+			bestBid = {name: books[i].name, price: Number(books[i].bids[0][0])}
+		}
+		if(Number(books[i].asks[0][0]) < bestAsk.price) {
+			bestAsk = {name: books[i].name, price: Number(books[i].asks[0][0])}
+		}
+	}
+	return [bestBid, bestAsk];
 }
 
 async function main() {
-	await Promise.all([
+	let books = await Promise.all([
 		kraken(),
 		ftx(),
 		binance(),
 		coinbase()
-	]);
-	//technically don't need to wait for all api calls to finish first and then determine best bid / ask. can do it as data arrives asynchronously
-	let bestBid = {
-		exchange: 'kraken',
-		price: Number(bids.kraken[0][0]),
-		amount: Number(bids.kraken[0][1])
-	};
-	for(const exchange in bids) {
-		if(Number(bids[exchange][0][0]) > bestBid.price) {
-			bestBid.exchange = exchange;
-			bestBid.price = Number(bids[exchange][0][0]);
-			bestBid.amount = Number(bids[exchange][0][1]);
-		}
-	}
-	let bestAsk = {
-		exchange: 'kraken',
-		price: Number(asks.kraken[0][0]),
-		amount: Number(asks.kraken[0][1])
-	};
-	for(const exchange in asks) {
-		if(Number(asks[exchange][0][0]) < bestAsk.price) {
-			bestAsk.exchange = exchange;
-			bestAsk.price = Number(asks[exchange][0][0]);
-			bestAsk.amount = Number(asks[exchange][0][1]);
-		}
-	}
-	//console.log(bestAsk, bestBid);
+	]); //technically don't need to wait for all api calls to finish first and then determine best bid / ask. can do it as data arrives asynchronously
+	const [bestBid, bestAsk] = best(books);
 	if(bestAsk.price < bestBid.price) {
 		console.log('arbitrage opportunity @', bestAsk, bestBid);
 	}
